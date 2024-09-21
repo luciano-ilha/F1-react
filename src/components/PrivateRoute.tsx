b24eb61d163ec15d9e0b22db2458daa12c29b4cb
Luciano Ilha <ilha.dev.br@gmail.com>
2024-09-21 18:49:03 -0300

    fix login

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,14 +1,14 @@
 import { Navigate } from 'react-router-dom'
 
 interface PrivateRouteProps {
-  element: React.ComponentType;
-  [key: string]: any;
+  element: React.ComponentType
+  [key: string]: any
 }
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ element: Component, ...rest }) => {
-  const isAuthenticated = !!localStorage.getItem('userId')
+  const isAuthenticated = !!localStorage.getItem('user')
 
-  return isAuthenticated ? (<Component {...rest} />) : (<Navigate to="/login" />)
+  return isAuthenticated ? <Component {...rest} /> : <Navigate to="/login" />
 }
 
 export default PrivateRoute
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,64 +4,67 @@ import { Link, useNavigate } from 'react-router-dom'
 const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
   const navigate = useNavigate()
 
-  const handleSubmit = async (e:any) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault()
 
     try {
-      // const response = await fetch('http://localhost:3000/users')
-      const response = [
-        { id: '1', name: 'Teste Nome', email: 'teste@teste.com', password: '12345' },
-        { id: '2', name: 'John Lark', email: 'john@john.com', password: '67890' },
-        { id: '3', name: 'Doe Forest', email: 'doe@doe.com', password: 'qwerty' }
-      ]
-      // const users = await response.json()
+      const response = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      })
 
-      const authenticatedUser = response.find((user:any) => user.email === email && user.password === password)
+      if (response.ok) {
+        const data = await response.json()
+        localStorage.setItem('user', JSON.stringify(data.user))
 
-      if (authenticatedUser) {
-        localStorage.setItem('authenticated', 'true')
-        localStorage.setItem('userId', authenticatedUser.id)
-        localStorage.setItem('userName', authenticatedUser.name)
         navigate('/')
       } else {
-        alert('E-mail ou senha incorretos')
+        const errorData = await response.json()
+        setError(errorData.error || 'Email ou senha incorretos.')
       }
     } catch (error) {
-      console.error('Erro de autenticação: ', error)
+      console.error('Erro de autenticação:', error)
+      setError('Erro de conexão com o servidor.')
     }
   }
 
   return (
-    <>
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="E-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        /> <br />
+        <button type="submit">Entrar</button>
+      </form>
+      
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      
       <div>
-        <div>
-          <h2>Login</h2>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              placeholder='E-mail'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              type="password"
-              placeholder='Senha'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            /> <br />
-            <button type='submit'>Entrar</button>
-          </form>
-          <div>
-            <p>Ainda não tem cadastro?</p>
-            <Link to='/'>
-              <span>Cadastrar</span>
-            </Link>
-          </div>
-        </div>
+        <p>Ainda não tem cadastro?</p>
+        <Link to='/'>
+          <span>Cadastrar</span>
+        </Link>
       </div>
-    </>
+    </div>
   )
 }
 
