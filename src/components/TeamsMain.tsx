7b6a013ffe1183fc612584af9648a9b0cca537bf
Luciano Ilha <ilha.dev.br@gmail.com>
2024-09-20 23:48:02 -0300

    change teams display info test

--- src/components/TeamsMain.tsx
+++ src/components/TeamsMain.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { useTeamsContext } from '../hooks/contexts/TeamsContext'
 import '../styles/TeamsMain.styles.css'
+import { TeamsObj } from '../types'
 
 const TeamsMain = () => { 
   const teams = useTeamsContext()
@@ -8,8 +9,16 @@ const TeamsMain = () => {
   return (
     <div className='flex flex-col justify-center teams-main-content'>
       <span className='font-semibold leading-6 text-white teams-main-title' style={{ marginBottom: '60px' }}>Teams</span>
-      <span className='font-semibold text-white' style={{ marginBottom: '15px' }}>Nome: {teams[0].name}</span>
-      <span className='font-semibold text-white' style={{ marginBottom: '15px' }}>Motor: {teams[0].engine}</span>
+      {teams.map((team: TeamsObj) => (
+        <>
+          <span className='font-semibold text-white' style={{ marginBottom: '10px' }}>Nome: {team.name}</span>
+          <span className='font-semibold text-white' style={{ marginBottom: '10px' }}>Cidade: {team.city}</span>
+          <span className='font-semibold text-white' style={{ marginBottom: '10px' }}>Voltas mais rápidas: {team.fastest_laps}</span>
+          <span className='font-semibold text-white' style={{ marginBottom: '10px' }}>Pole positions: {team.pole_positions}</span>
+          <span className='font-semibold text-white' style={{ marginBottom: '10px' }}>Campeonatos Mundiais: {team.world_championships}</span>
+          <span className='font-semibold text-white' style={{ marginBottom: '35px' }}>Pneus: {team.tyres}</span>
+        </>
+      ))}
     </div>
   )
 }
